4d3d040e131cad5e88cdf2684baaf1303a5cbca8
Beau Dobbin <beau@beaunus.com>
2018-02-13 17:04:12 +0900

    Initial commit

---
+++ src/eiken_json_to_reaper.py
@@ -0,0 +1,150 @@
+#! /usr/local/bin/python3
+# coding=utf-8
+"""A module to generate a REAPER Clip Splicer json file from a .txt file
+specification.
+"""
+
+import argparse
+import json
+import sys
+import jsonschema
+import eiken_script_to_json
+from beaunus_clip_splicer_tools import make_media_item, make_track, make_region
+
+PAUSES = {
+    'after_choice': 0.2,
+    'after_three_choices': 0.2,
+    'after_number': 0.2,
+    'after_prompt': 0.2,
+    'after_question': 0.2,
+    'after_sentence': 0.2,
+    'beginning_of_track': 0.2
+}
+
+
+def parse_args():
+    """Parse and return the command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description=('''Generate a REAPER Clip Splicer json file from a
+        .json file specification.'''))
+    parser.add_argument('-v', action='store_true', help='Show verbose output')
+    parser.add_argument(
+        '-schema', dest='schema', help='JSON schema to validate against')
+    parser.add_argument(
+        '-script_file',
+        help='The file that represents the Eiken listening test information.')
+    args = parser.parse_args()
+    return args
+
+
+def get_eiken_reaper_object(eiken_object):
+    result = make_region(
+        name=eiken_object['title'], path=eiken_object['path'])
+    for section in eiken_object['sections']:
+        this_section = make_region(
+            name=section['name'])
+        for question in section['questions']:
+            this_question = make_track(
+            name='Question ' + question['question_number'],
+            pre_track_pause_length=PAUSES['beginning_of_track'])
+            for i in range(2):
+                for line in question['lines']:
+                    line_type = line['type']
+                    if line_type == 'silent':
+                        continue
+
+                    # If this is a choice with a number, add the spoken number.
+                    if line_type == 'choice':
+                        if 'choice_number' in line:
+                            this_component = make_media_item(
+                                name='I/' + line['choice_number'],
+                                track='I',
+                                filename='I/' + line['choice_number'])
+                            this_question['components'].append(this_component)
+                            this_component = make_media_item(
+                                name='Pause after_number',
+                                track='Pauses',
+                                length=PAUSES['after_number'])
+                            this_question['components'].append(this_component)
+
+                    # If this is a question, add the 'Question' prompt.
+                    if line_type == 'question':
+                        this_component = make_media_item(
+                            name='I/Question.',
+                            track='I',
+                            filename='I/Question')
+                        this_question['components'].append(this_component)
+                        this_component = make_media_item(
+                            name='Pause after_choice',
+                            track='Pauses',
+                            length=PAUSES['after_choice'])
+                        this_question['components'].append(this_component)
+
+                    # Add text for main component.
+                    if 'vocalist' in line:
+                        vocalist = line['vocalist']
+                    else:
+                        vocalist = 'I'
+                    this_component = make_media_item(
+                        name=vocalist + '/' + line['text'],
+                        track=vocalist,
+                        filename=vocalist + '/' + line['text'])
+                    this_question['components'].append(this_component)
+
+                    pause_name = 'after_' + line_type
+                    pause_after = make_media_item(
+                        name='Pause ' + pause_name,
+                        track="Pauses",
+                        length=PAUSES[pause_name])
+                    this_question['components'].append(pause_after)
+
+                pause_after = make_media_item(
+                    name='Pause after_question',
+                    track="Pauses",
+                    length=PAUSES['after_question'])
+                this_question['components'].append(pause_after)
+
+            this_section['components'].append(this_question)
+        result['components'].append(this_section)
+    return result
+    # Create a region for the whole project
+    # Create a region for each section
+    # Create a track for each question.
+    # Create the components for each question.
+
+
+def main():
+    """Reads the command-line arguments and prints the JSON representation of
+    the data for this project.
+    """
+    args = parse_args()
+    if args.script_file:
+        eiken_object = eiken_script_to_json.read_file_into_object(
+            args.script_file)
+    else:
+        eiken_object = json.loads(sys.stdin.read())
+
+    if args.v:
+        print(
+            json.dumps(obj=eiken_object, ensure_ascii=False,
+                       sort_keys=False, indent=2),
+            file=sys.stderr)
+
+    eiken_reaper_object = get_eiken_reaper_object(
+        eiken_object)
+
+    # Validate the output
+    if args.schema:
+        with open(args.schema) as schema_file:
+            schema = json.load(schema_file)
+            jsonschema.validate(eiken_reaper_object, schema)
+
+    eiken_reaper_json = json.dumps(
+        obj=eiken_reaper_object, ensure_ascii=False, sort_keys=False, indent=2)
+
+    print(eiken_reaper_json)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/eiken_script_to_json.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python
+
+'''A module for parsing an Eiken Script text file into a REAPER Clip
+Splicer JSON string.
+'''
+
+import argparse
+import json
+import random
+import re
+
+
+def parse_args():
+    '''Parses and returns the command line arguments.
+    '''
+    parser = argparse.ArgumentParser(
+        description='''Parse the given file into a json file that
+        represents it.''')
+    parser.add_argument('filename',
+                        help='''The file to parse.
+                        The file should be a .txt file whose format is 
+                        specified in README.md. ''')
+    args = parser.parse_args()
+    return args
+
+
+def read_file_into_object(filename):
+    '''Reads an Eiken listening test file into an Python dictionary and
+    returns the object.
+
+    Args:
+        filename: The file that describes the Eiken listening test.
+
+    Returns:
+        An object that represents the Eiken listening test.
+    '''
+    with open(filename, encoding='utf-8') as script_file:
+        lines = script_file.readlines()
+        # Determine the project title.
+        project_title = lines.pop(0).partition('//')[0].strip()
+        # Initialize a list of sections to be parsed later.
+        sections = []
+
+        # Iterate over the lines, creating a new list of lines for each
+        # section within the text.
+        this_section = None
+        for line in lines:
+            # Ignore // comments
+            line = line.partition('//')[0].strip()
+            # Ignore empty lines
+            if not line:
+                continue
+            if line.startswith('[Section]'):
+                # This line begins a new section.
+                if this_section is not None:
+                    sections.append(this_section)
+                this_section = {}
+                title = line.partition('[Section]')[2].strip()
+                this_section['title'] = title
+                this_section['lines'] = list()
+            else:
+                this_section['lines'].append(line)
+        # Add the most recently created section to the result.
+        if this_section is not None:
+            sections.append(this_section)
+    return parse_sections(project_title, sections)
+
+
+def parse_sections(project_title, sections):
+    '''Parses a list of sections and returns an Python dictionary that
+    contains the same information.
+
+    Args:
+        project_title: The title of the project.
+        sections: A list of sections.
+
+    Returns:
+        A dict that contains all the information in the sections.
+    '''
+    result = {}
+    result['title'] = project_title
+    result['path'] = 'clips'
+    components = []
+    for section in sections:
+        components.append(parse_section(section))
+    result['sections'] = components
+    return result
+
+
+def parse_section(section):
+    '''Parses a list of lines that represent a section and returns an
+    object.
+
+    Args:
+        section: A list of lines for each section.
+
+    Returns:
+        A dict that contains all the data for the given section.
+    '''
+    # Initialize the result.
+    result = {}
+    result['name'] = section['title']
+
+    # Initialize a list of questions that belong to this section.
+    questions = []
+
+    # Iterate over all the lines and parse them accordingly.
+    this_question = None
+    for line in section['lines']:
+        if line.startswith('#'):
+            # This line contains question-specific data.
+            # E.g. the question number and optional vocalist genders.
+            if this_question is not None:
+                # Add the previously read question to the result.
+                questions.append(parse_question(this_question))
+
+            # Initialize a new question to contain the following lines.
+            this_question = {}
+            number_and_vocalists = line.partition('#')[2].strip()
+            partition = number_and_vocalists.partition('[')
+            this_question['question_number'] = partition[0]
+            if partition[2]:
+                # If the vocalists are specified, parse them.
+                vocalist_list = partition[2].partition(']')[0].split(',')
+            else:
+                # Randomize the vocalists.
+                vocalist_list = ['M', 'F']
+                random.shuffle(vocalist_list)
+            # Create a map of vocalist names.
+            # 'A' and 'B' --> genders 'M' and 'F'
+            vocalists = {}
+            # pylint: disable=consider-using-enumerate
+            for i in range(len(vocalist_list)):
+                vocalists[chr(65 + i)] = vocalist_list[i]
+            this_question['vocalists'] = vocalists
+            # Initialize a list of lines that will be added later.
+            this_question['lines'] = list()
+        else:
+            # This is not the first line in a question.
+            # Simply add the line to the result.
+            this_question['lines'].append(line)
+    # Parse and add the most recently read question.
+    if this_question is not None:
+        questions.append(parse_question(this_question))
+    result['questions'] = questions
+    return result
+
+
+def parse_question(question):
+    '''Parse a question and return an object.
+
+    Args:
+        question: A list of lines to be parsed.
+
+    Returns:
+        A dict containing all the data for the question.
+    '''
+    # Initialize a list of spoken lines for this question.
+    lines = []
+    for line in question['lines']:
+        if line.startswith('[Question]'):
+            # This line contains the text for a spoken question.
+            line = line.partition('[Question]')[2].strip()
+            lines.append({'type': 'question', 'text': line.strip()})
+        elif line.startswith('[Choices - Not spoken]'):
+            # The following lines are unspoken choices.
+            line_type = 'silent'
+        elif line.startswith('[Choices]'):
+            # The following lines are spoken choices.
+            line_type = 'choice'
+        else:
+            choice_number = None
+            if re.search(r'\d', line):
+                # This line begins with a digit --> It is a choice.
+                partition = line.partition('.')
+                choice_number = partition[0]
+                line = partition[2]
+            else:
+                # This line does not begin with a digit --> It is not a choice.
+                line_type = 'prompt'
+            if re.search(r'.*:', line):
+                # This line has a colon --> There is a specified vocalist.
+                partition = line.partition(':')
+                vocalist = partition[0].strip()
+                line = partition[2]
+            line_object = {'type': line_type, 'text': line.strip()}
+            # If the vocalist is specified, look up its gender.
+            if vocalist:
+                line_object['vocalist'] = question['vocalists'][vocalist]
+            # If there is a choice number, add its data to the result.
+            if choice_number:
+                line_object['choice_number'] = choice_number
+            lines.append(line_object)
+    result = {
+        'question_number': question['question_number'],
+        'lines': lines
+    }
+    return result
+
+
+def main():
+    '''Parse the given file and print a JSON representation of Python
+    dict object that represents the same data.
+    '''
+    args = parse_args()
+    test_object = read_file_into_object(args.filename)
+    print(json.dumps(test_object, indent=4, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    main()
