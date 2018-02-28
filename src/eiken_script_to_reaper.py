7220fc86a3f569e08d54748bb4e3c413718e68cc
Beau Dobbin <beau@beaunus.com>
2018-02-28 20:41:41 +0900

    Encapsulate get_section()

--- src/eiken_script_to_reaper.py
+++ src/eiken_script_to_reaper.py
@@ -38,6 +38,78 @@ def parse_args():
     return args
 
 
+def get_section(section):
+    """Given a dict that represents a section from an Eiken test,
+    returns a beaunus_clip_splicer region that contains all the clips
+    for that section.
+    """
+    this_section = make_region(
+        name=section['name'])
+    for question in section['questions']:
+        this_question = make_track(
+            name='Question ' + question['question_number'],
+            pre_track_pause_length=PAUSES['beginning_of_track'])
+        for _ in range(2):
+            for line in question['lines']:
+                line_type = line['type']
+                if line_type == 'silent':
+                    continue
+
+                # If this is a choice with a number, add the spoken number.
+                if line_type == 'choice':
+                    if 'choice_number' in line:
+                        this_component = make_media_item(
+                            name='I/' + line['choice_number'],
+                            track='I',
+                            filename='I/' + line['choice_number'])
+                        this_question['components'].append(this_component)
+                        this_component = make_media_item(
+                            name='Pause after_number',
+                            track='Pauses',
+                            length=PAUSES['after_number'])
+                        this_question['components'].append(this_component)
+
+                # If this is a question, add the 'Question' prompt.
+                if line_type == 'question':
+                    this_component = make_media_item(
+                        name='I/Question.',
+                        track='I',
+                        filename='I/Question')
+                    this_question['components'].append(this_component)
+                    this_component = make_media_item(
+                        name='Pause after_choice',
+                        track='Pauses',
+                        length=PAUSES['after_choice'])
+                    this_question['components'].append(this_component)
+
+                # Add text for main component.
+                if 'vocalist' in line:
+                    vocalist = line['vocalist']
+                else:
+                    vocalist = 'I'
+                this_component = make_media_item(
+                    name=vocalist + '/' + line['text'],
+                    track=vocalist,
+                    filename=vocalist + '/' + line['text'])
+                this_question['components'].append(this_component)
+
+                pause_name = 'after_' + line_type
+                pause_after = make_media_item(
+                    name='Pause ' + pause_name,
+                    track="Pauses",
+                    length=PAUSES[pause_name])
+                this_question['components'].append(pause_after)
+
+            pause_after = make_media_item(
+                name='Pause after_question',
+                track="Pauses",
+                length=PAUSES['after_question'])
+            this_question['components'].append(pause_after)
+
+        this_section['components'].append(this_question)
+    return this_section
+
+
 def get_eiken_reaper_object(eiken_object):
     '''Given a dict that represents an eiken test, returns a dict that
     represents a beaunus_clip_splicer object.
@@ -46,71 +118,7 @@ def get_eiken_reaper_object(eiken_object):
         name=eiken_object['title'], path=eiken_object['path'])
     # pylint: disable = too-many-nested-blocks
     for section in eiken_object['sections']:
-        this_section = make_region(
-            name=section['name'])
-        for question in section['questions']:
-            this_question = make_track(
-                name='Question ' + question['question_number'],
-                pre_track_pause_length=PAUSES['beginning_of_track'])
-            for _ in range(2):
-                for line in question['lines']:
-                    line_type = line['type']
-                    if line_type == 'silent':
-                        continue
-
-                    # If this is a choice with a number, add the spoken number.
-                    if line_type == 'choice':
-                        if 'choice_number' in line:
-                            this_component = make_media_item(
-                                name='I/' + line['choice_number'],
-                                track='I',
-                                filename='I/' + line['choice_number'])
-                            this_question['components'].append(this_component)
-                            this_component = make_media_item(
-                                name='Pause after_number',
-                                track='Pauses',
-                                length=PAUSES['after_number'])
-                            this_question['components'].append(this_component)
-
-                    # If this is a question, add the 'Question' prompt.
-                    if line_type == 'question':
-                        this_component = make_media_item(
-                            name='I/Question.',
-                            track='I',
-                            filename='I/Question')
-                        this_question['components'].append(this_component)
-                        this_component = make_media_item(
-                            name='Pause after_choice',
-                            track='Pauses',
-                            length=PAUSES['after_choice'])
-                        this_question['components'].append(this_component)
-
-                    # Add text for main component.
-                    if 'vocalist' in line:
-                        vocalist = line['vocalist']
-                    else:
-                        vocalist = 'I'
-                    this_component = make_media_item(
-                        name=vocalist + '/' + line['text'],
-                        track=vocalist,
-                        filename=vocalist + '/' + line['text'])
-                    this_question['components'].append(this_component)
-
-                    pause_name = 'after_' + line_type
-                    pause_after = make_media_item(
-                        name='Pause ' + pause_name,
-                        track="Pauses",
-                        length=PAUSES[pause_name])
-                    this_question['components'].append(pause_after)
-
-                pause_after = make_media_item(
-                    name='Pause after_question',
-                    track="Pauses",
-                    length=PAUSES['after_question'])
-                this_question['components'].append(pause_after)
-
-            this_section['components'].append(this_question)
-        result['components'].append(this_section)
+        result['components'].append(get_section(section))
     return result
 
 
